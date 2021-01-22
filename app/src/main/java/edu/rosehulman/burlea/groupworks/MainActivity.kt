a6d51eb08ff6a345720ae7f8e543050d35e3879b
Aditya Burle <aburle369@gmail.com>
2021-01-21 21:41:05 -0500

    removed testing stuff

--- app/src/main/java/edu/rosehulman/burlea/groupworks/MainActivity.kt
+++ app/src/main/java/edu/rosehulman/burlea/groupworks/MainActivity.kt
@@ -2,7 +2,6 @@ package edu.rosehulman.burlea.groupworks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
 
