3ab1921ac91d6e8474b493324fba1c909dfcee1c
Aditya Burle <aburle369@gmail.com>
2021-01-28 23:37:20 -0500

    connected login screen to see task view

--- app/src/main/java/edu/rosehulman/burlea/groupworks/LoginFragment.kt
+++ app/src/main/java/edu/rosehulman/burlea/groupworks/LoginFragment.kt
@@ -6,25 +6,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.fragment_login.*
 
 
 class LoginFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
     override fun onAttach(context: Context){
         super.onAttach(context)
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        authenticateButton.setOnClickListener{
+            val activity: AppCompatActivity = (context as AppCompatActivity)
+            activity.supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment,TaskListFragment()).commit()
+        }
     }
 
 }
