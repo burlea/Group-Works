6dfc1a6e4f3d4406f3c76648c172ca62e2c3957f
Aditya Burle <aburle369@gmail.com>
2021-01-29 00:22:44 -0500

    added sign out feature

--- app/src/main/java/edu/rosehulman/burlea/groupworks/MainActivity.kt
+++ app/src/main/java/edu/rosehulman/burlea/groupworks/MainActivity.kt
@@ -3,6 +3,7 @@ package edu.rosehulman.burlea.groupworks
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
@@ -11,21 +12,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        loginButton.setOnClickListener {
-            val ft = supportFragmentManager.beginTransaction()
-            ft.replace(R.id.nav_host_fragment, LoginFragment())
-            ft.commit()
-        }
-
-        signInButton.setOnClickListener {
-            val ft = supportFragmentManager.beginTransaction()
-            ft.replace(R.id.nav_host_fragment, SignInFragment())
-            ft.commit()
-        }
+        val ft = supportFragmentManager.beginTransaction()
+        ft.replace(R.id.nav_host_fragment, StartPage())
+        ft.commit()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.sign_out -> signOut()
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun signOut(): Boolean {
+        val ft = supportFragmentManager.beginTransaction()
+        ft.replace(R.id.nav_host_fragment, StartPage())
+        ft.commit()
         return true
     }
 }
