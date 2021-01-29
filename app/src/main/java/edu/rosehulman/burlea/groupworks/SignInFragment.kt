0349bb6174925f76f4ba89277a023cde480df13d
Aditya Burle <aburle369@gmail.com>
2021-01-28 23:40:41 -0500

    connected setup screen to view tasks screen

--- app/src/main/java/edu/rosehulman/burlea/groupworks/SignInFragment.kt
+++ app/src/main/java/edu/rosehulman/burlea/groupworks/SignInFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.fragment_login.*
+import kotlinx.android.synthetic.main.sign_up.*
 
 class SignInFragment : Fragment() {
 
@@ -18,5 +21,13 @@ class SignInFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.sign_up, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        signInAuthentication.setOnClickListener{
+            val activity: AppCompatActivity = (context as AppCompatActivity)
+            activity.supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment,TaskListFragment()).commit()
+        }
     }
 }
