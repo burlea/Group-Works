14635e5237ccd70eb9338fe35c5aa01b5f08dc3a
ritenojb <ritenojb@rose-hulman.edu>
2021-02-07 21:00:50 -0500

    Added User model object

---
+++ app/src/main/java/edu/rosehulman/burlea/groupworks/User.kt
@@ -0,0 +1,8 @@
+package edu.rosehulman.burlea.groupworks
+
+data class User(
+    val username: String = "",
+    val password: String = "",
+    val name: String = "",
+    val email: String = ""
+)
